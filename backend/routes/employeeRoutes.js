6458602dac25293450b2298cd8721ff4c8de18c6
Filip Dosenovic <filipdosenovic@Filips-MacBook-Pro.local>
2023-01-23 23:03:23 -0700

    Adding CRUD for Employees

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const {
   createEmployee,
   delEmployee,
+  getEmployeesByCompany,
+  updateEmployeeTable,
 } = require("../controllers/employeeController");
 router.post("/create", createEmployee);
-router.delete("/delete", delEmployee);
+router.put("/delete", delEmployee);
+router.get("/", getEmployeesByCompany);
+router.put("/update", updateEmployeeTable);
+
 module.exports = router;
