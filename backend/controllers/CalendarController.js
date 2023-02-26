03299667e0edf6242f54f332e3f2e7d4fea7ae64
Filip Dosenovic <filipdosenovic@Filips-MacBook-Pro.local>
2023-02-25 23:22:09 -0700

    Got the calendar to display the bookings. Although there are a few bugs when it comes to viewing the weekly view
    Calander able to view bookings. Needs attention regarding weekly view

--- backend/controllers/CalendarController.js
+++ backend/controllers/CalendarController.js
@@ -1,6 +1,24 @@
 const getCalendar = async (req, res) => {
   try {
-    res.status(200).json({ data: [{ serviceName: "Haircut", employeeName: "John", startTime: "12:00:00", endTime: "12:30:00", date: "2023-02-22" }] });
+    const data = [
+      { serviceName: "Haircut", employeeName: "Filip", startTime: "12:00:00", endTime: "12:30:00", date: "2023-02-22" },
+      { serviceName: "Haircut", employeeName: "john", startTime: "12:00:00", endTime: "12:30:00", date: "2023-02-23" },
+      { serviceName: "Haircut", employeeName: "john", startTime: "12:00:00", endTime: "12:30:00", date: "2023-02-13" },
+      { serviceName: "Haircut", employeeName: "john", startTime: "12:00:00", endTime: "12:30:00", date: "2023-02-01" },
+    ];
+
+    const convertedData = data.map((item, index) => {
+      const startTime = new Date(`${item.date}T${item.startTime}`);
+      const endTime = new Date(`${item.date}T${item.endTime}`);
+      return {
+        id: index + 1,
+        title: `${item.serviceName} With ${item.employeeName}`,
+        start: startTime,
+        end: endTime,
+        employee: item.employeeName,
+      };
+    });
+    res.status(200).json({ data: convertedData });
   } catch (error) {}
 };
 
